c6d945e47ec243d0ec0d9c5e1142c79ee63bbd1e
olamug <kuberska.aleksandra@gmail.com>
2019-12-12 19:39:36 +0100

    'update'

--- brudnopis_tkinter.py
+++ brudnopis_tkinter.py
@@ -7,3 +7,11 @@ askquestion
 akokcancel
 askyesno
 askretrycancel
+
+stackabuse.com/author/mateusz
+wiki.python.org/moin/TkInter
+tkdocs.com/tutorial
+tkinter.unpythonic.net/wiki
+pmw.sourceforge.net
+effbot.org/tkinterbook (2.7)
+thinkingtkinter.sourceforge.net (2.7)
